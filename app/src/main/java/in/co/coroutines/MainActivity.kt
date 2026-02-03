29baf5192adee90920dc215e5703cd1a6bacbdc5
VikasGrow <development@precisiongrow.co.in>
2026-02-03 17:32:49 +0530

    coroutine builder with launch

--- app/src/main/java/in/co/coroutines/MainActivity.kt
+++ app/src/main/java/in/co/coroutines/MainActivity.kt
@@ -5,48 +5,44 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
 
-    private val suspendingFunction = SuspendingFunction()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val job = CoroutineScope(Dispatchers.IO).launch {
+        // lifecycleScope is tied to Activity lifecycle (safe, no memory leaks)
+        lifecycleScope.async {
             Log.d("CoroutineDemo", "Main coroutine started")
+
+            // Calling suspend function
+            printFollowers()
+
+            Log.d("CoroutineDemo", "Main coroutine ended")
+        }
+    }
+
+    // This must be suspend because it calls another suspend function
+    private suspend fun printFollowers() {
+
+        // Switch to IO thread for network / DB work
+        val fbFollowers = withContext(Dispatchers.IO) {
+            getFbFollowers()
         }
 
-//        job.join()
-        Log.d("CoroutineDemo", "Main coroutine ended")
+        // This runs on Main thread again
+        Log.d("CoroutineDemo", "Facebook Followers: $fbFollowers")
+    }
 
-
-// Suspending Function class call
-        // Coroutine 1
-//        lifecycleScope.launch {
-//            Log.d("CoroutineDemo", "Main coroutine 1 started")
-//
-//            withContext(Dispatchers.IO) {
-//                suspendingFunction.task1()
-//            }
-//
-//            Log.d("CoroutineDemo", "Main coroutine 1 ended")
-//        }
-
-        // Coroutine 2
-//        lifecycleScope.launch {
-//            Log.d("CoroutineDemo", "Main coroutine 2 started")
-//
-//            withContext(Dispatchers.IO) {
-//                suspendingFunction.task2()
-//            }
-//
-//            Log.d("CoroutineDemo", "Main coroutine 2 ended")
-//        }
+    // Simulating network call
+    private suspend fun getFbFollowers(): Int {
+        delay(200) // pretend API delay
+        return 52
     }
 }
